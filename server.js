6e53e3bcfca4567c4cbb821430347a9b968b2761
rasheedarahimi <rasheedarahimi10@outlook.com>
2023-03-13 14:54:32 +0000

    part two done

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ const port = 3000;
 
 //TODO: Create your GET Request Route Below: 
 app.get('/restaurants', async (req, res) => {
-    const restaurants = await Restaurant.findAll();
-    res.json();
+    try {
+        const restaurants = await Restaurant.findAll();
+        res.json(restaurants)
+    }catch(err){
+        console.log(err);
+    }
 })
 
 app.get('/restaurants/:id', async (req, res) => {
